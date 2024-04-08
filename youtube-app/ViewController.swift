b95349486b57c064750aa80977c8db5719b528d3
Hailey Oh <haileyoh@usc.edu>
2024-04-07 23:03:18 -0700

    initial xcode project creation

---
+++ youtube-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-app
+//
+//  Created by Hailey Oh on 4/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
